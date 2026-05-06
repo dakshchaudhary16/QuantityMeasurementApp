22054eb9626aa4178bad8744175686668ac42f9b
Your Name <kxzarx97@gmail.com>
2026-05-06 15:13:59 +0530

    UC2: Added Inches class and separate equality checks for feet and inches

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,50 +1,79 @@
 /**
  * Quantity Measurement App
  *
- * Version 1.0
- * UC1: Feet Measurement Equality
+ * Version 2.0
+ * UC2: Feet and Inches Equality
  */
 
 public class QuantityMeasurementApp {
 
-    // -------- INNER CLASS --------
+    // -------- FEET CLASS --------
     static class Feet {
 
         private final double value;
 
-        // Constructor
         public Feet(double value) {
             this.value = value;
         }
 
-        // Override equals method
         @Override
         public boolean equals(Object obj) {
 
-            // Same reference check (Reflexive)
             if (this == obj) return true;
 
-            // Null or different class check
             if (obj == null || getClass() != obj.getClass()) return false;
 
-            // Type cast
             Feet other = (Feet) obj;
 
-            // Compare values safely
             return Double.compare(this.value, other.value) == 0;
         }
     }
 
-    // -------- MAIN METHOD --------
+    // -------- INCHES CLASS --------
+    static class Inches {
+
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // -------- STATIC METHODS --------
+
+    public static boolean compareFeet(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInches(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+
+    // -------- MAIN --------
     public static void main(String[] args) {
 
         System.out.println("=== Quantity Measurement App ===");
 
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+        // Feet comparison
+        System.out.println("Feet Equal? " + compareFeet(1.0, 1.0));
 
-        boolean result = f1.equals(f2);
-
-        System.out.println("Are both equal? " + result);
+        // Inches comparison
+        System.out.println("Inches Equal? " + compareInches(1.0, 1.0));
     }
 }
